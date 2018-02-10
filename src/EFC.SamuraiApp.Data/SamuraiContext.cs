6e8a56c664a9ae6d91eccff5cb17a0fef26ec536
Fernando Araujo <naando.araujo@gmail.com>
2018-02-10 19:41:23 -0200

    Fix project references + add LoggerFactory to Data project

--- src/EFC.SamuraiApp.Data/SamuraiContext.cs
+++ src/EFC.SamuraiApp.Data/SamuraiContext.cs
@@ -1,10 +1,20 @@
 ﻿using EFC.SamuraiApp.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace EFC.SamuraiApp.Data
 {
     public class SamuraiContext : DbContext
     {
+        public static readonly LoggerFactory ConsoleLoggerFactory = new LoggerFactory(
+            new [] { new ConsoleLoggerProvider(
+                (category, level) => category == DbLoggerCategory.Database.Command.Name && 
+                                     level    == LogLevel.Information, true
+            )}    
+        );
+
+
         #region Ctor
 
         public SamuraiContext(DbContextOptions<SamuraiContext> options)
@@ -25,13 +35,17 @@ namespace EFC.SamuraiApp.Data
 
         #region Override Methods
 
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    //base.OnConfiguring(optionsBuilder);
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
 
-        //    var connStr = "Server=(localdb)\\mssqllocaldb; Database=SamuraiAppCore; Trusted_Connection=True;";
-        //    optionsBuilder.UseSqlServer(connStr);
-        //}
+            //var connStr = "Server=(localdb)\\mssqllocaldb; Database=SamuraiAppCore; Trusted_Connection=True;";
+            //optionsBuilder.UseSqlServer(connStr);
+
+            optionsBuilder
+                //.UseSqlServer(connStr)
+                .UseLoggerFactory(ConsoleLoggerFactory);
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
